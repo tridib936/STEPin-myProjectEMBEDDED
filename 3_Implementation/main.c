46b63b020f0a83a900580523df581b12a31d0a58
tridib936 <61946877+tridib936@users.noreply.github.com>
2021-09-16 17:51:07 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,37 @@
+/**
+ * @file main.c
+ * @author Tridib Mazumder
+ * @brief Seat heater for a vehicle seat
+ * @version 0.1
+ * @date 2021-09-16
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "activity1.h"
+#include "activity2.h"
+#include "activity3.h"
+#include "activity4.h"
+
+int main(void)
+{
+    uint16_t temp;
+    while(1)
+    {
+        if(activity1_LED()==1) //Check if both the switches are pressed
+        {
+            TurnLED_ON();//Turn LED ON
+            temp=activity2_GetADC(); //Get the ADC value
+            activity3_PWM(temp); //PWM output based on temperature
+		    activity4_USARTWrite(temp); //To Serial monitor to print Temperature
+        }
+        else  //in all other cases
+        {
+            TurnLED_OFF();//Turn LED OFF
+		    _delay_ms(200);
+        }
+
+    }
+    return 0;
+}
